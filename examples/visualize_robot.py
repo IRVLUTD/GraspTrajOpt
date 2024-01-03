bea82defeff194c1f7f1882a40b2e98e3d6a756e
Yu Xiang <yuxiang@cs260574.utdallas.edu>
2024-01-02 22:31:09 -0600

    visualize fetch

---
+++ examples/visualize_robot.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import argparse
+import pathlib
+
+cwd = pathlib.Path(__file__).parent.resolve()  # path to current working directory
+sys.path.append(os.path.join(cwd, ".."))
+import optas
+from optas.visualize import Visualizer
+
+
+def make_args():
+    parser = argparse.ArgumentParser(
+        description="Generate grid and spawn objects", add_help=True
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default="fetch",
+        help="robot model name",
+    )   
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = make_args()
+    model = args.model
+
+    if model == "lwr":
+        urdf_filename = os.path.join(cwd, "robots", "kuka_lwr", "kuka_lwr.urdf")
+        robot_model = optas.RobotModel(urdf_filename=urdf_filename)
+
+    elif model == "lbr":
+        xacro_filename = os.path.join(cwd, "robots", "kuka_lbr", "med7.urdf.xacro")
+        robot_model = optas.RobotModel(xacro_filename=xacro_filename)
+
+    elif model == "fetch":
+        urdf_filename = os.path.join(cwd, "robots", "fetch", "fetch.urdf")
+        print(urdf_filename)
+        robot_model = optas.RobotModel(urdf_filename=urdf_filename)
+
+    vis = Visualizer(camera_position=[3, 3, 3])
+    vis.grid_floor()
+    vis.robot(
+        robot_model,
+        q=robot_model.get_random_joint_positions(),
+        show_links=True,
+        display_link_names=True,
+        link_names_alpha=0.4,
+    )
+
+    save = False
+    if save:
+        vis.save('vis.png')
+    else:
+        vis.start()
