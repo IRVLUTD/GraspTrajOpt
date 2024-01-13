fix ik rotation error bug

examples/pybullet_scenes.py
@@ -411,17 +411,19 @@ if __name__ == '__main__':
 
             # test IK for remaining grasps
             n = RT_grasps_world.shape[0]
+            RT_grasps_base = RT_grasps_world.copy()
             found_ik = np.zeros((n, ), dtype=np.int32)
             q0 = np.array(env.robot.q()).reshape((env.robot.ndof, 1))
             for i in range(n):
                 RT = RT_grasps_world[i].copy()
                 # change world to robot base
                 RT[:3, 3] -= base_position
+                RT_grasps_base[i] = RT.copy()
                 q_solution, err_pos, err_rot = ik_solver.solve_ik(q0, RT)
                 if err_pos < 0.01 and err_rot < 5:
                     found_ik[i] = 1
-                    print(RT)
             RT_grasps_world = RT_grasps_world[found_ik == 1] 
+            RT_grasps_base = RT_grasps_base[found_ik == 1] 
             print('Among %d grasps, %d found IK' % (n, np.sum(found_ik)))
             if RT_grasps_world.shape[0] == 0:
                 continue

gto/ik_solver.py
@@ -68,7 +68,7 @@ class IKSolver:
         err_pos = np.linalg.norm(RT[:3, 3] - tf[:3, 3])
         quat1 = mat2quat(RT[:3, :3])
         quat2 = mat2quat(tf[:3, :3])
-        err_rot = np.arccos(2 * np.square(np.dot(quat1, quat2)) - 1) * 180 / np.pi
+        err_rot = np.arccos(np.clip(2 * np.square(np.dot(quat1, quat2)) - 1, -1, 1)) * 180 / np.pi
 
         print("***********************************") 
         print("Casadi IK solution:")
@@ -86,7 +86,7 @@ def make_args():
         "-r",
         "--robot",
         type=str,
-        default="fetch",
+        default="panda",
         help="Robot name",
     )
     args = parser.parse_args()
@@ -128,12 +128,7 @@ if __name__ == "__main__":
         RT = np.array([[-0.61162336,  0.79089652,  0.01998741,  0.46388378],
             [ 0.7883297,   0.6071185,   0.09971584, -0.15167381],
             [ 0.06673018,  0.07674521, -0.99481508,  0.22877409],
-            [ 0.,          0.,          0.,          1.        ]]) 
-        
-        RT = np.array([[ 0.90725112,  0.35363723, -0.22767539,  0.46844359],
-        [ 0.39880392, -0.89527687,  0.19858144, -0.2570503 ],
-        [-0.13360674, -0.27096108, -0.95327284,  0.22880854],
-        [ 0.,          0.,         0.,          1.        ]])
+            [ 0.,          0.,          0.,          1.        ]])
 
     else:
         print(f'robot {robot_name} not supported')